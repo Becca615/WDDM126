7a93893ef404738f579a1bbf82502893ccb93298
Becca615 <damilolarebecca8@gmail.com>
2025-04-16 20:26:19 -0400

    About Page

---
+++ Project 2/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <h2>About Me</h2>
+    <p>
+      Graphic designer with a passion for creating visually stunning designs.
+      Specializes in logo design, branding, and web design.
+    </p>
+  </section>
+);
+
+export default About;
