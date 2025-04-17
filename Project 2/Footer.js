a0af5bb76e05859f497899d2784a0edfabaf8f4f
Becca615 <damilolarebecca8@gmail.com>
2025-04-16 20:27:57 -0400

    Footer

---
+++ Project 2/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <p>&copy; 2023 Your Name</p>
+    <small>This site was built using HTML, CSS, and JavaScript.</small>
+  </footer>
+);
+
+export default Footer;
